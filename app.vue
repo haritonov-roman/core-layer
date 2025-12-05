4b5900821cd2a6652e60ff22b5afc083ea9c323d
Харитонов Роман Сергеевич <roman.kharitonov@vitech.team>
2025-12-05 17:35:36 +0700

    Add app.vue and UiHeader.vue

---
+++ app.vue
@@ -0,0 +1,27 @@
+<script setup>
+import UiHeader from './components/UiHeader'
+</script>
+
+<template>
+  <div class="layout">
+    <UiHeader />
+    <div class="main">
+      <NuxtPage />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+
+.main {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  padding: 16px;
+}
+</style>
---
+++ components/UiHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header">
+    Header
+  </div>
+</template>
+
+<style scoped>
+.header {
+  font-size: 24px;
+  font-weight: 700;
+  padding: 16px;
+  border: 1px solid black;
+}
+</style>
+<script setup>
+</script>
